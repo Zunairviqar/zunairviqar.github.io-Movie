0caa7a26074d6db2891ba30521695c034aeffd77
Usman <usmanali.irshad@gmail.com>
2020-06-22 06:27:10 +0500

    added cast page

---
+++ cast.js
@@ -0,0 +1,6 @@
+let hackBg = document.getElementById("hacker").contentDocument;
+let popCast = hackBg.getElementById("confidential");
+popCast.addEventListener('click', () => {
+    console.log("button is clicked");
+    // window.open("Main.html");
+	});
