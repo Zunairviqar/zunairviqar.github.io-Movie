6ead16cf5a42f647cd1b0daa51e12a8e01b6916d
Zunairviqar <66274735+Zunairviqar@users.noreply.github.com>
2020-06-24 04:00:01 +0500

    fixes

--- Working/About.js
+++
@@ -1,9 +0,0 @@
-// $(document).ready(function(){
-//   $("#check").click(function(){
-//     setTimeout(function () {
-//       $("#Movie").toggle();
-//       $("#footer").toggle();
-//
-//     }, 100);
-//   });
-// });
